c9f88d8cf7c3431e9660f5074f57f1eb243b0c96
Ridhima Sakhuja <ridhimasakhuja66@gmail.com>
2024-06-01 15:19:59 -0400

    updates to pdf

--- server/pdf.py
+++ server/pdf.py
@@ -7,26 +7,35 @@ openaiClient = OpenAI(api_key=OPENAI_API_KEY)
 file_path = 'BILLS-118s1444is.pdf'
 
 def extract_text_from_pdf(file_path):
-    text = ""
-    with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        for page in reader.pages:
-            text += page.extract_text()
-    return text
+    file = openaiClient.files.create(
+        file=open(file_path, "rb"),
+        purpose="fine-tune"
+    )
+
+    file_id = file.id
+    content = openaiClient.files.content(file_id)
+
+    with open(file_path, "rb") as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+        text_content = ""
+        for page in pdf_reader.pages:
+            text_content += page.extract_text()
+    return text_content
+
 
 def get_bill_answers(text, question):
     response = openaiClient.chat.completions.create(
-        model="gpt-3.5-turbo-16k",
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": question},
             {"role": "user", "content": text}
         ]
     )
-    
     answer = response.choices[0].message.content
     return answer
 
 
-pdf_content = extract_text_from_pdf(file_path=file_path)
-answer = get_bill_answers(pdf_content, "What is this bill about?")
+text_content = extract_text_from_pdf(file_path=file_path)
+answer = get_bill_answers(text_content, "What is this bill about?")
 print(answer)
